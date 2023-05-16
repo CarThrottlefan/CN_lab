eed12b2fba0610eb8e8eb85fdcea387aabbfc48b
CarThrottlefan <alexandervici@gmail.com>
2023-05-16 02:49:00 +0200

    first test, it starts

--- a3/a3.py
+++ a3/a3.py
@@ -1,6 +1,6 @@
 import threading
 import socket
-import sys
+import os
 import re
 from time import sleep
 
@@ -16,10 +16,10 @@ print("This is the server IP: ", serverIp)
 #listen for connections
 numOfUsers = 64 # change for more users to be supported
 serverSocket.listen(numOfUsers) # starts listening for connections
-usernames = { } # set of usernames
+usernames = {'Alex'} # set of usernames
 
-def send(sock, msg):
-    """Sends entire message over socket."""
+def send(sock, msg): # sends whole message over socket
+
 
     string_bytes = (msg + '\n').encode("utf-8", "replace")
     bytes_len = len(string_bytes)
@@ -27,23 +27,36 @@ def send(sock, msg):
 
     while num_bytes_to_send > 0:
         num_bytes_to_send -= sock.send(string_bytes [bytes_len - num_bytes_to_send:])
+        
+def recv(sock):
+    """Waits until entire message is received from server (until \\n) and returns it"""
+    data = sock.recv(4096)
+    msg = data.decode("utf-8") # decodes the sent data
+
+    return msg
     
 def user_handle(client_sock, client_address):
     while True:
         try: 
-            data = serverSocket.recv()
+            data = serverSocket.recv(client_sock)
             msg = data.split(" ", 1) # splits the received data into command and TXT
             cmd = msg[0], txt = msg[1]
-            
-            #match data:
-                
+    
+            match data:
+                case 'LIST\n':
+                    usernames = str(usernames)
+                    data.send(client_sock, ('List of current users: ' + usernames))
+        except:
+            print('Error')
+              
   
 
 def main():
     while True:
         print('Server has started...\n')
+        print('Usernames: ', usernames)
         client_sock, client_address = serverSocket.accept()
-        data = client_sock.recv()
+        data = recv(client_sock)
         msg = data.split(" ", 1)
         
         if data[1] in usernames:
@@ -54,6 +67,10 @@ def main():
                  
         server_thread = threading.Thread(target= user_handle, args=(client_sock, client_address))
         server_thread.start()
+        
+if __name__ == "__main__":
+    main()
+    
 
 
 
