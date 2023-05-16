4988534f27bb28ef16bdd70e0cdfadd795543625
CarThrottlefan <alexandervici@gmail.com>
2023-05-16 18:15:09 +0200

    Handshake+list works, tested with multiple users

--- a3/a3.py
+++ a3/a3.py
@@ -13,14 +13,12 @@ data = ""
 
 print("This is the server IP: ", serverIp)
 
-#listen for connections
 numOfUsers = 64 # change for more users to be supported
 serverSocket.listen(numOfUsers) # starts listening for connections
-usernames = {'Alex\n'} # set of usernames
+usernames = {'Alex'} # set of usernames
+user_sockets = [] #list of user sockets
 
 def send(sock, msg): # sends whole message over socket
-
-
     string_bytes = (msg + '\n').encode("utf-8", "replace")
     bytes_len = len(string_bytes)
     num_bytes_to_send = bytes_len
@@ -39,56 +37,60 @@ def user_handle(client_sock, client_address):
     while True:
         try: 
             data = recv(client_sock)
-            msg = data.split(" ", 1) # splits the received data into command and TXT
-            cmd = msg[0], txt = msg[1]
+            print('Data from client is ' + data)
+            msg = data.split(" ", 1)# splits the received data into command and TXT
+            msg_set = set(msg)
+            
+            if len(msg) == 1:
+                cmd = msg[0]
+            elif len(msg) == 3:
+                cmd = msg[0]
+                username = msg[1]
+                txt = msg[2]  
+            else:
+                cmd = msg[0], txt = msg[1]
 
             match cmd:
                 case 'LIST\n':
-                    usernames = str(usernames)
-                    send(client_sock, ('List of current users: ' + usernames))
+                    users = str(usernames) #converts the set of users to a string
+                    #print(users)
+                    string = 'List of current users: ' + users
+                    send(client_sock, string)
+                    
+                #case cmd if cmd.startswith('SEND'):
+                    
+                
                 case cmd:
                     continue
+                
         except:
             print('Error 1')
             os._exit(1)
-              
-  
 
 def main():
     while True:
         print('Server has started...\n')
         print('Usernames: ', usernames)
         client_sock, client_address = serverSocket.accept()
+        user_sockets.append(client_sock)
+        
         data = recv(client_sock)
-        print('Data from client is ' + data)
         msg = data.split(" ", 1) #this contains the list with [command, msg]
         name = msg[1]
-        print("Name is " + name)
-        #print(msg)
-        #print(msg[1])
+        name = name.replace('\n', '') #removes \n from the name to be put in the set
         
-        if msg[1] in usernames:
+        global numOfUsers #FIXME implement a way to check if max num of connections is not reached
+        
+        if name in usernames:
             send(client_sock,"IN-USE")
             main()
         else:
-            #print('Data = ' + data[0])
-            string = "HELLO " + msg[1]
-            print(string)
+            string = "HELLO " + name
             send(client_sock, string)
-            usernames.add(msg[1])
+            usernames.add(name)
                  
         server_thread = threading.Thread(target= user_handle, args=(client_sock, client_address))
         server_thread.start()
         
 if __name__ == "__main__":
     main()
-    
-
-
-
-
-
-
-
- 
-
