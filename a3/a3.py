56e4fa86a1865ab0cbaec3786ceec46415e8c3da
CarThrottlefan <alexandervici@gmail.com>
2023-05-22 16:26:31 +0200

    Final version

--- a3/a3.py
+++ a3/a3.py
@@ -18,10 +18,11 @@ data = ""
 
 print("This is the server IP: ", serverIp)
 
-numOfUsers = 64 # change for more users to be supported
-serverSocket.listen(numOfUsers) # starts listening for connections
+maxUsers = 4 # change for more users to be supported
+serverSocket.listen(maxUsers) # starts listening for connections
 dummyUser = userData(serverSocket, 'Server')
 userSet = {dummyUser} # set of usernames
+numOfUsers = 0 # number of connected users
 
 
 def send(sock, msg): # sends whole message over socket
@@ -41,88 +42,89 @@ def recv(sock):
     
 def user_handle(client_sock, sender_name):
     while True:
-        try: 
-            #if input == '!quit': #FIXME implement a quit/shutdown function for the server
-                #os._exit(1)
-            
+        try:  
             userFound = False
             data = recv(client_sock)
             
+            if not data: # when a client disconnects
+                print(sender_name + ' has closed connection\n')
+                for user in userSet:
+                    if user.sock == client_sock:
+                        
+                        userSet.discard(user)
+                        global numOfUsers
+                        numOfUsers -= 1
+                        break
+                break
 
             print('Data from client is ' + data)
             msg = data.split(" ", 2)# splits the received data into command and TXT
-            msg_set = set(msg)
+            print(msg)
             
             if len(msg) == 1:
                 cmd = msg[0]
             elif len(msg) == 3:
-                cmd = msg[0]
-                receiver_name = msg[1]
-                txt = msg[2]
-                  
+                cmd, receiver_name, txt = msg
             else:
-                cmd = msg[0], txt = msg[1]
+                cmd, txt = msg
+                receiver_name = ''
 
             match cmd:
-                case 'LIST\n':
+                case 'LIST\n': # handles the list request from the client
                     user_names = {user.alias for user in userSet}
                     users = str(user_names)
-                    #print('It gets here 2')
-                    #users = str(usernames) #converts the set of users to a string
-                    #print(users)
                     string = 'List of current users: ' + users
                     send(client_sock, string)
                     
-                case cmd if cmd.startswith('SEND'):
-                    #print('It gets here')
-                    for user in userSet:
-                        if user.alias == receiver_name:
-                            receiver_sock = user.sock
-                            txt = str(txt)
-                            txt = 'DELIVERY ' +  str(sender_name) + ' ' + txt
-                            send(client_sock, 'SEND-OK')
-                            send(receiver_sock, txt)
-                            userFound = True
+                case cmd if cmd.startswith('SEND'): # handles the message command
+                    receiver_name = str(receiver_name)
+                    if receiver_name == '': #this is a concession, the empty field is 'Message' - this was done to not make another variable, since 'txt' is what follows 'cmd'
+                        send(client_sock, 'BAD-RQST-BODY') # sent when the client doesn't specify a message
+                        
+                    else:
+                        for user in userSet:
+                            if user.alias == receiver_name:
+                                receiver_sock = user.sock
+                                txt = 'DELIVERY ' +  str(sender_name) + ' ' + txt
+                                send(client_sock, 'SEND-OK')
+                                send(receiver_sock, txt)
+                                userFound = True
                             
                     if not userFound:
-                        print("Is it entering here?")
                         send(client_sock, 'BAD-DEST-USER')
                    
                 case cmd:
-                    continue
+                    send(client_sock, 'BAD-RQST-HDR') # if the client sends something that is not a command
                 
-        except:
-            print('Error 1')
-            os._exit(1)
+        except OSError as e:
+            print("Error: " + e)
+            main()
 
 def main():
     while True:
         print('Server has started...\n')
-        #print('Usernames: ', usernamesSet)
-        print(type(userSet))
         client_sock, client_address = serverSocket.accept()
         
         data = recv(client_sock)
         msg = data.split(" ", 1) #this contains the list with [command, msg]
         name = msg[1]
         name = name.replace('\n', '') #removes \n from the name to be put in the set
+        global maxUsers, numOfUsers 
         
-        global numOfUsers #FIXME implement a way to check if max num of connections is not reached
+        if(numOfUsers == maxUsers - 1):
+            send(client_sock, 'BUSY')
+            main()
         
-        #if name in userNames:
-            #send(client_sock,"IN-USE")
-            #main()
-            
         if any(user.alias == name for user in userSet):
             send(client_sock,"IN-USE")
             main()
+            
         else:
+            numOfUsers += 1
+            print(name + ' has joined\n')
             user_class = userData(client_sock, name)
             userSet.add(user_class)
-            #user_tuple = (client_sock, name) #generates a tuple of a user's name+socket
-            #print(usernamesSet)
-            #user_info.append(user_tuple) #appends each new user's tuple to a list of connected user's tuples
-            #print(user_info)
+            
             string = "HELLO " + name
             send(client_sock, string)
                  
@@ -130,5 +132,6 @@ def main():
         server_thread.start()
         
 if __name__ == "__main__":
+    
     main()
     
